28d84308308b28873aed2e541e4410119481b48e
Nikhil Yekhe <nikhil.yekhe@ing.com>
2022-06-11 19:13:04 +0200

    day 7

--- day7.ts
+++ day7.ts
@@ -1,81 +1,99 @@
 import { delay } from "https://deno.land/std@0.142.0/async/mod.ts";
 const p = console.log;
-const raw = await Deno.readTextFile("7.1.in");
+const raw = await Deno.readTextFile("7.in");
+const numOfThreads = 5;
+const fixed = 60;
 const lines = raw.trim().split("\n");
 
-class PrioQueue<T> {
-  comp: (a: T, b: T) => number;
-  queue: T[];
-  autosort: boolean
-  constructor(comp: (a: T, b: T) => number, autosort = true) {
-    this.comp = comp;
-    this.queue = [];
-    this.autosort = autosort
-  }
-  push(item: T): void {
-    if (this.queue.includes(item)) return;
-    this.queue.push(item);
-    if (this.autosort)
-      this.queue.sort(this.comp);
-  }
-  isPresent(): boolean {
-    return this.queue.length > 0;
-  }
-  sort(): void {
-    this.queue.sort(this.comp)
-  }
-
-  next(): T {
-    if (this.queue.length > 0) {
-      const item = this.queue.shift();
-      if (item) return item;
-      throw new Error("no items found");
-    } else {
-      throw new Error("empty list");
-    }
-  }
-}
-
-const part1 = async (input: string[]) => {
-  // build map
-  const map: Map<string, string[]> = lines
+const parse = (
+  input: string[],
+): [Map<string, string[]>, Map<string, number>, string[]] => {
+  const count: Map<string, number> = new Map();
+  const map: Map<string, string[]> = new Map();
+  lines
     .map((line) =>
       line.replace("Step ", "").replace(" must be finished before step ", "")
-        .replace(" can begin.", "")
-    )
-    .reduce((acc, v) => {
-      const next = acc.get(v[1]) ?? [];
-      next.push(v[0]);
-      next.sort();
-      acc.set(v[1], next);
-      return acc;
-    }, new Map());
-  // add starting points
-  map.forEach((v, k) => {
-    v.filter((e) => !map.has(e)).forEach((e) => map.set(e, []));
+        .replace(" can begin.", "").split("")
+    ).forEach(([k, v]) => {
+      const val = map.get(k) ?? [];
+      val.push(v);
+      val.sort();
+      const c = count.get(v) ?? 0;
+      count.set(v, c + 1);
+      if (count.get(k) === undefined) count.set(k, 0);
+      map.set(k, val);
+    });
+  const Q: string[] = [];
+  count.forEach((v, k) => {
+    if (v === 0) Q.push(k);
   });
-  p(map);
-  const visited: Set<string> = new Set();
-  const pq: PrioQueue<string> = new PrioQueue<string>((a: string, b: string) =>
-    a.charCodeAt(0) - b.charCodeAt(0), false
-  );
-  map.forEach((v, k) => pq.push(k))
-  pq.sort()
-  let ans = ""
-  while(pq.isPresent() && visited.size < map.size){
-    const curr = pq.next()
-    const req = map.get(curr) ?? []
-    // p({curr, req})
-    // await delay(1000)
-    if (!req.some(e => !visited.has(e))) {
-      ans += curr
-      visited.add(curr)
-      pq.sort()
-    } else {
-      pq.push(curr)
-    }
-  }
-  p(ans)
+  // p({ map, count, Q });
+  return [map, count, Q];
 };
 
-await part1(lines);
+const part1 = (input: string[]) => {
+  const [map, count, Q] = parse(input);
+  let ans = "";
+  while (Q.length > 0) {
+    Q.sort();
+    const curr = Q.shift();
+    if (curr === undefined) {
+      p("error");
+      break;
+    }
+    ans += curr;
+    const vals = map.get(curr);
+    if (vals === undefined) break;
+
+    vals.forEach((v) => {
+      count.set(v, (count.get(v) ?? 0) - 1);
+      if ((count.get(v) ?? 1000) === 0) {
+        Q.push(v);
+      }
+    });
+  }
+  p(ans);
+};
+
+const part2 = (input: string[]) => {
+  const [map, count, Q] = parse(input);
+  const EV: [string, number][] = [];
+  let ans = 0;
+  const start_work = () => {
+    while (EV.length < numOfThreads && Q.length > 0) {
+      Q.sort();
+      const curr = Q.shift();
+      if (curr === undefined) {
+        p("error");
+        break;
+      }
+      EV.push([curr, ans + fixed + curr.charCodeAt(0) - 64]);
+    }
+    // EV.sort((a, b) => a[0].charCodeAt(0) - b[0].charCodeAt(0))
+  };
+  start_work();
+  while (EV.length > 0) {
+    // EV.sort((a, b) => a[0].charCodeAt(0) - b[0].charCodeAt(0))
+    EV.sort((a, b) => a[1] - b[1]);
+    const curr = EV.shift();
+    if (curr === undefined) {
+      p("err");
+      break;
+    }
+    ans = curr[1];
+    const vals = map.get(curr[0]);
+    if (vals === undefined) break;
+    vals.forEach((v) => {
+      count.set(v, (count.get(v) ?? 0) - 1);
+      if ((count.get(v) ?? 1000) === 0) {
+        Q.push(v);
+      }
+    });
+    start_work();
+    // p({curr,vals, Q, EV})
+  }
+  p(ans);
+};
+
+part1(lines);
+part2(lines);
