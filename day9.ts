0135367c0f3775c6f5a2c9a2f43309f0f285ed42
Nik <yendenikhil@gmail.com>
2022-06-18 01:52:50 +0200

    day 9

---
+++ day9.ts
@@ -0,0 +1,106 @@
+const p = console.log
+const raw = "431 players; last marble is worth 7095000 points"
+// const raw = "09 players; last marble is worth 25 points"
+const num_users = parseInt(raw.split(' ')[0])
+const num_marbles = parseInt(raw.split(' ')[6])
+
+class Node {
+  val: number
+  prev: Node | null
+  next: Node | null
+  constructor(val: number) {
+    this.val = val
+    this.prev = null
+    this.next = null
+  }
+}
+
+class DLL {
+  node: Node
+  first: Node
+  last: Node
+  constructor(val: number) {
+    const node = new Node(val)
+    this.node = node
+    this.first = node
+    this.last = node
+  }
+  push(val: number) {
+    const nn = new Node(val)
+    nn.next = this.node.next
+    this.node.next = nn
+    nn.prev = this.node
+    if (nn.next !== null) {
+      nn.next.prev = nn
+    } else {
+      this.last = nn
+    }
+  }
+  pop(): number {
+    const val = this.node.val
+    if (this.node.prev !== null) {
+      this.node.prev.next = this.node.next
+    }  else if (this.node.next !== null){
+      this.first = this.node.next
+    }
+    
+    if (this.node.next !== null) {
+      this.node.next.prev = this.node.prev
+    } else if (this.node.prev !== null){
+      this.last = this.node.prev
+    }
+    return val
+  }
+  next() {
+    if (this.node.next === null) {
+      this.node = this.first
+    } else {
+      this.node = this.node.next
+    }
+  }
+  prev() {
+    if (this.node.prev === null) {
+      this.node = this.last
+    } else {
+      this.node = this.node.prev
+    }
+  }
+  toString() {
+    let curr = this.first
+    let ans = ""
+    while(true) {
+      ans += curr.val + " "
+      if (curr.next === null) break
+      curr = curr.next
+    }
+    return ans 
+  }
+}
+const buff = new DLL(0)
+const scores = []
+for (let i = 0; i < num_users; i++) {
+  scores[i] = 0
+}
+
+for (let i = 1; i <= num_marbles; i++) {
+  if (i % 23 === 0) {
+    buff.prev() 
+    buff.prev() 
+    buff.prev() 
+    buff.prev() 
+    buff.prev() 
+    buff.prev() 
+    const val = buff.pop()
+    // p({i, val})
+    scores[i % num_users] += (val + i)
+  } else { 
+    buff.next()
+    buff.next()
+    buff.push(i)
+  }
+  // p(buff.toString())
+}
+
+const max = (a: number, b: number) => a > b ? a : b
+
+p(scores.reduce(max))
